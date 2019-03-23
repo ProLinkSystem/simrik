54892e3fd43a61321b19dd2116b5ab3607b5be15
ProLinkSystem <prolinksystem01@gmail.com>
2019-03-23 17:45:22 +0545

    Initial commit

---
+++ resources/views/layouts/front/home-slider.blade.php
@@ -0,0 +1,13 @@
+<section id="hero" class="hero-section top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="hero-content">
+                    <h1 class="hero-title">Holiday <br> Collection</h1>
+                    <a class="btn btn-success" href="#" target="_blank" role="button">Shop Now <i class="fa fa-shopping-bag"></i></a>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+</section>
